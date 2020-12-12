4f130a65f0e94182249c4f0fef4342f07379e722
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-12-12 10:32:57 -0500

    SalariedEmployee completed

--- src/ca/durhamcollege/SalariedEmployee.java
+++ src/ca/durhamcollege/SalariedEmployee.java
@@ -1,4 +1,24 @@
 package ca.durhamcollege;
 
-public class SalariedEmployee {
+import java.time.LocalDate;
+
+public class SalariedEmployee extends Employee {
+    protected double annualSalary;
+
+    public SalariedEmployee(String fullName, LocalDate birthDate, String id, double salary){
+        super(fullName, birthDate, id);
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double calculatePayDay(){
+        return annualSalary / 52;
+    }
 }
